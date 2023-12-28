718cf8ff270ffdfc6ed5a98e45024d90f2955dce
DmitPloh <kroll.88@mail.ru>
2023-12-28 11:42:32 +0300

    add laptops

---
+++ Laptop.java
@@ -0,0 +1,32 @@
+public class Laptop {
+    private String name;
+    private int totalRAM;
+    private int totalHDD;
+    private String operSys;
+    private int price;
+
+   
+    public Laptop(String name, int totalRAM, int totalHDD, String operSys, int price) {
+        this.name = name;
+        this.totalRAM = totalRAM;
+        this.totalHDD = totalHDD;
+        this.operSys = operSys;
+        this.price = price;
+    }
+
+    // public void addLaptop (String name, int totalRAM, int totalHDD, String operSys, String color, int price) {
+    //     this.name = name;
+    //     this.totalRAM = totalRAM;
+    //     this.totalHDD = totalHDD;
+    //     this.operSys = operSys;
+    //     this.color = color;
+    //     this.price = price;
+    // }
+
+    @Override
+    public String toString() {
+        return "name: " + name + ", RAM: " + totalRAM + ", Hard: " + totalHDD + ", System: " + operSys + ", price: " + price;
+    }
+
+
+}
--- main.java
+++ main.java
@@ -1,5 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class main {
     public static void main(String[] args) {
-        
+        Set<Laptop> laptops = new HashSet<>();
+        laptops.add(new Laptop("HP", 16, 512, "Windows 10", 20000));
+        laptops.add(new Laptop("Dell", 16, 256, "Linux", 25000));
+        laptops.add(new Laptop("Lenovo", 8, 256, "Windows 10", 18000));
+        laptops.add(new Laptop("MSI", 16, 1024, "Windows 11", 28000));
+        laptops.add(new Laptop("HP", 32, 1024, "Windows 11", 32000));
+
+        printSet(laptops);
     }
+
+    static void printSet(Set<Laptop> laptops){
+        for (Laptop laptop : laptops) {
+            System.out.println(laptop);
+        }
+    }
+
+    
 }
